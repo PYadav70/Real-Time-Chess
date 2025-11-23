5b530361da4545e48c290bf1355f3bfbf34d6979
PYadav70 <technoyadav1234@gmail.com>
2025-11-24 01:32:09 +0530

    Add game initialization and move handling to WebSocket server

---
+++ backend/dist/GameManager.js
@@ -0,0 +1,44 @@
+import { INIT_GAME, MOVE } from "./messages.js";
+import { Game } from "./Game.js";
+export class GameManager {
+    games;
+    pendingUser;
+    users;
+    constructor() {
+        this.games = [];
+        this.pendingUser = null;
+        this.users = [];
+    }
+    addUser(socket) {
+        this.users.push(socket);
+        this.addHandler(socket);
+    }
+    removeUser(socket) {
+        this.users = this.users.filter(user => user !== socket);
+    }
+    addHandler(socket) {
+        socket.on("message", (data) => {
+            const message = JSON.parse(data.toString());
+            if (message.type === INIT_GAME) {
+                if (this.pendingUser) {
+                    // start the game
+                    const game = new Game(this.pendingUser, socket);
+                    this.games.push(game);
+                    this.pendingUser = null;
+                }
+                else {
+                    this.pendingUser = socket;
+                }
+            }
+            if (message.type === MOVE) {
+                console.log("inside move");
+                const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
+                if (game) {
+                    console.log("inside make move");
+                    game.makeMove(socket, message.move);
+                }
+            }
+        });
+    }
+}
+//# sourceMappingURL=GameManager.js.map
--- backend/src/Game.ts
+++ backend/src/Game.ts
@@ -1,12 +1,13 @@
 import { Chess } from "chess.js";
 import { WebSocket } from "ws";
-import { GAME_OVER } from "./messages.js";
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages.js";
 
 export  class Game {
     public player1: WebSocket;
     public player2: WebSocket;
     public board : Chess;
     private startTime: Date;
+    private moveCount = 0;
 
     constructor(player1:WebSocket, player2:WebSocket){
         this.player1 = player1;
@@ -14,6 +15,18 @@ export  class Game {
         this.board = new Chess();
         // this.moves = [];
         this.startTime = new Date();
+        this.player1.send(JSON.stringify({
+            type: INIT_GAME,
+            payload:{
+                color:"white"
+            }
+        }))
+        this.player2.send(JSON.stringify({
+            type: INIT_GAME,
+            payload:{
+                color:"black"
+            }
+        }))
     }
    
 
@@ -21,25 +34,51 @@ export  class Game {
         from :string;
         to : string;
     }){
-       if(this.board.moves.length % 2 === 0 && socket !== this.player1){
+       if(this.moveCount % 2 === 0 && socket !== this.player1){
         return 
        }
-       if(this.board.moves.length % 2 === 0 && socket !== this.player2){
+       if(this.moveCount % 2 === 1 && socket !== this.player2){
         return 
        }
+       console.log("did not early return")
         try {
-            this.board.move(move)
+            this.board.move(move);
+            this.moveCount++;
         } catch (error) {
+            console.log(error);
             return
         }
+        console.log("move succeeded")
 
         if(this.board.isGameOver()){
             //SEND THE GAME OVER MESSAGE TO BOTH PLAYER
             this.player1.emit(JSON.stringify({
-                type : GAME_OVER
+                type : GAME_OVER,
+                payload:{
+                    winner:this.board.turn() === "w" ? "black" : "white"
+                }
+            }))
+            this.player1.emit(JSON.stringify({
+                type: GAME_OVER,
+                payload:{
+                    winner: this.board.turn() === "w" ? "black" : "white"
+                }
             }))
             return
         }
+
+        if(this.moveCount % 2 === 0){
+            this.player2.send(JSON.stringify({
+                type:MOVE,
+                payload: move
+            }))
+        }else{
+            this.player1.send(JSON.stringify({
+                type: MOVE,
+                payload: move
+            }))
+        } 
+        this.moveCount++;
 
     }
    
